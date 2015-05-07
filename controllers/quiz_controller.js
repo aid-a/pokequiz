93c9ebb692315f9f4177e15578c171b8dbe8b69c
aid-a <aida.jtarifa@alumnos.upm.es>
2015-05-07 16:08:12 +0200

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,7 +10,7 @@ exports.question = function(req, res){
 //GET /quizes/answer
 exports.answer = function(req, res){
 	models.Quiz.findAll().success(function(quiz){ 
-		if (req.query.respuesta === 'Ketchum'){
+		if (req.query.respuesta === quiz[0].respuesta){
 			res.render('quizes/answer', {respuesta: "Correcto, qué friki eres."});
 		} else {
 			res.render('quizes/answer', {respuesta: 'Incorrecto :('});
