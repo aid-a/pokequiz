af27e669655cf4a7825d2f1804428c3b3f4d6278
aid-a <aida.jtarifa@alumnos.upm.es>
2015-05-18 17:05:02 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,7 +3,7 @@ var models = require('../models/models.js')
 //GET /quizes/:id
 exports.show = function(req, res){
 	res.render('quizes/show', { quiz: req.quiz, errors: []});
-};
+};	// req.quiz: instancia de quiz cargada con autoload
 
 //GET /quizes/:id/answer
 exports.answer = function(req, res){
@@ -63,4 +63,28 @@ exports.load = function(req, res, next, quizId){
       } else{next(new Error('No existe quizId=' + quizId))}
     }
   ).catch(function(error){next(error)});
+};
+
+// GET /quizes/:id/edit
+exports.edit = function(req, res) {
+  var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
+
+  res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+  req.quiz.pregunta  = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz.validate().then(function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
+    }
+  );
 };
